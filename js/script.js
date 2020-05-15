8bf261a54a89dffb0fde43e87aabcc4c638bd0d7
patszy <patszy97@interia.pl>
2020-05-15 14:21:55 +0200

    Added login/register selection, created unfinished register form.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onload = () => {
     const inputs = document.querySelectorAll('.input');
     const eye = document.querySelector('.label .far.fa-eye');
+    const door = document.querySelectorAll('.door')[0];
+    const register = document.querySelectorAll('.section-register')[0];
+    const login = document.querySelectorAll('.section-login')[0];
 
     inputs.forEach(input => {
         input.addEventListener("focus", (event) => {
@@ -13,7 +16,20 @@ window.onload = () => {
     });
 
     eye.addEventListener("click", (event) => {
+        event.preventDefault();
         let input = event.target.previousElementSibling;
         (input.type == "password") ? input.type = "text" : input.type = "password";
+    });
+
+    door.querySelector('.door-register').addEventListener("click", (event) => {
+        door.classList.add('login');
+        register.style.display = 'block';
+        login.style.display = 'none';
+    });
+
+    door.querySelector('.door-login').addEventListener("click", (event) => {
+        door.classList.remove('login');
+        register.style.display = 'none';
+        login.style.display = 'block';
     });
 }
